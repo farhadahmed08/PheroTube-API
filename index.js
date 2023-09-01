0698752fe58b34bf5f5c57161c17df529ada9b2b
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-09-01 20:24:18 +0800

    naavbar has done

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const handleLoadData = async (categoryId) => {
     const res = await fetch (`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
     const data = await res.json();
     const singleCard = data.data;
-    console.log(singleCard)
+    // console.log(singleCard)
     const cardContainer = document.getElementById('card-container')
      cardContainer.innerText = ''
     
@@ -31,8 +31,8 @@ const handleLoadData = async (categoryId) => {
         <div class="card bg-base-100 shadow-xl">
         <figure>
           <img
-            src=${element.thumbnail}
-            alt="Shoes"
+            src=${element?.thumbnail}
+            alt="thumbnail"
           />
         </figure>
         <div class="card-body">
@@ -43,23 +43,21 @@ const handleLoadData = async (categoryId) => {
                 <div class="avatar online">
                   <div class="w-14 rounded-full ">
                     <img
-                      src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                    src=${element?.authors[0].profile_picture}
                     />
                   </div>
                 </div>
               </div>
               <div>
-                <h2 class="card-title">Biden Pledges Nearly $3 To Ukraine </h2>
+                <h2 class="card-title">${element.title} </h2>
                 <div class="flex gap-2 items-center">
-                    <h6>Jimmy Dane </h6>
+                    <h6>${element?.authors[0].profile_name} </h6>
                 <p><img src="fi_10629607.svg" alt="" srcset=""></p>
                 </div>
-                <small>91K views</small>
+                <small>${element.others.views} views</small>
               </div>
             </div>
-            <div class="card-detaild-btn">
-              
-            </div>
+             </div>
           </div>
         </div>
       </div>
@@ -71,4 +69,6 @@ const handleLoadData = async (categoryId) => {
     
 }
 
-handleTube()
+handleTube();
+
+handleLoadData('1000');
